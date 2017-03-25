0913883ea8b47b7f3f1d45efa86cc603e0d8578a
Parshva Jain <parsh.cosmos11@gmail.com>
2017-03-25 22:22:40 +0530

    Release 2.0.0 #MajorChange

--- Server2/routes/data.js
+++ Server2/routes/data.js
@@ -1,8 +1,17 @@
 var express = require('express');
-var winRate = require('../json/winRate.json')
+
+var heroes = require('../json/heroes.json');
+var types = require('../json/type.json');
+var winRate = require('../json/winRate.json');
 
 var router = express.Router();
 
+router.get('/heroes.json', function(req, res, next) {
+    res.send(heroes);
+});
+router.get('/types.json', function(req, res, next) {
+    res.send(types);
+});
 router.get('/winRate.json', function(req, res, next) {
     res.send(winRate);
 });
--- routes/data.js
+++ routes/data.js
@@ -1,10 +1,19 @@
 var express = require('express');
-var heroes = require('../json/heroes.json')
+
+var heroes = require('../json/heroes.json');
+var types = require('../json/type.json');
+var winRate = require('../json/winRate.json');
 
 var router = express.Router();
 
 router.get('/heroes.json', function(req, res, next) {
     res.send(heroes);
+});
+router.get('/types.json', function(req, res, next) {
+    res.send(types);
+});
+router.get('/winRate.json', function(req, res, next) {
+    res.send(winRate);
 });
 
 module.exports = router;
